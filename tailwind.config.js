45df1ba80d5cd83e89746e5e100eca18fff8dc3e
Genaro Garcia <genaro.dev@outlook.com>
2023-07-31 14:59:05 -0500

    install tailwind forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,15 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          "Marine blue": "hsl(213, 96%, 18%)",
-          "Purplish blue": "hsl(243, 100%, 62%)",
-          "Pastel blue": "hsl(228, 100%, 84%)",
-          "Light blue": "hsl(206, 94%, 87%)",
-          "Strawberry red": "hsl(354, 84%, 57%)",
+          "Marine-blue": "hsl(213, 96%, 18%)",
+          "Purplish-blue": "hsl(243, 100%, 62%)",
+          "Pastel-blue": "hsl(228, 100%, 84%)",
+          "Light-blue": "hsl(206, 94%, 87%)",
+          "Strawberry-red": "hsl(354, 84%, 57%)",
         },
         neutral: {
-          "Cool gray": "hsl(231, 11%, 63%)",
-          "Light gray": "hsl(229, 24%, 87%)",
+          "Cool-gray": "hsl(231, 11%, 63%)",
+          "Light-gray": "hsl(229, 24%, 87%)",
           Magnolia: "hsl(217, 100%, 97%)",
           Alabaster: "hsl(231, 100%, 99%)",
         },
@@ -29,5 +29,5 @@ module.exports = {
       Ubuntu: ["Ubuntu", "sans-serif"],
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
 };
